802d8bc46e115491395cd9999d8289e4bcd05490
DongHee Kim <opklnm102@gmail.com>
2016-09-04 10:57:59 +0900

    push logic modify

--- 5safe/routes/index.js
+++ 5safe/routes/index.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var controllerPush = require('../controller/controllerPush');
+var db = require('../util/db.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var push = new controllerPush();
-    push.sendPushMessageFromGcm(new Array('aaa', 'bbb', 'ccc'));
+
+    var sql = 'SELECT device_model FROM user';
+    db.query(sql, function(err, result){
+        if(err){
+            res.status(500);
+        }else{
+            console.log(result);
+            var push = new controllerPush();
+            push.sendPushMessageFromGcm(result);
+        }
+    })
   res.render('index', { title: 'Express' });
 
 });
